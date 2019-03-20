5bfc33f6242bdd04c6801702686729498d40fc85
Everaldo <everaldofront@gmail.com>
2019-03-20 17:00:11 -0300

    Módulos

---
+++ calc.js
@@ -0,0 +1,9 @@
+const SomaFunc = require("./somar");
+const SomaFunc = require("./sub");
+const SomaFunc = require("./multi");
+const SomaFunc = require("./div");
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(10,5));
+console.log(MultiFunc(100,10));
+console.log(DivFunc(8,2));
