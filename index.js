ce94e7d5723f6868dbe5c8d384a3fa3760f1caa5
Everaldo <everaldofront@gmail.com>
2019-03-21 12:08:41 -0300

    Exibindo HTML

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const app = express();
 
 
 app.get("/", function(req, res){
-    res.send("Seja bem-vindo ao meu app!");
+    res.sendFile(__dirname + "/html/index.html");
 });
 
 app.get("/sobre", function(req, res){
-    res.send("Minha página sobre");
+    res.sendFile(__dirname + "/html/sobre.html");
 });
 
 app.get("/blog", function(req, res){
