b3bed60f73a4bb39f0713bf980b215dc0de85eb4
Everaldo <everaldofront@gmail.com>
2019-03-21 17:58:50 -0300

    Manipulando o MySQL

--- teste.sql
+++ teste.sql
@@ -3,3 +3,12 @@ CREATE TABLE usuarios(
 	email VARCHAR(100),
 	idade INT
 );
+
+/* Retorna todos usuários com idade igual a 8 */
+SELECT * usuarios WHERE idade = 8; 
+
+/* Retorna todos usuários com nome igual a Maria */
+SELECT * usuarios WHERE nome = "Maria"; 
+
+/* Retorna todos usuários com idade maior ou igual a 18 */
+SELECT * usuarios WHERE idade >= 18; 
