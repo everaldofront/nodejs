5bea5dda21808dffb105b13b182fd24ba5c8f7e0
Everaldo <everaldofront@gmail.com>
2019-03-22 12:00:31 -0300

    Models no Sequelize

--- bdteste.js
+++ bdteste.js
@@ -4,8 +4,21 @@ const sequelize = new Sequelize('bdteste', 'root', 'drinfer9', {
     dialect: 'mysql'
 })
 
+/*
 sequelize.authenticate().then(function(){
     console.log("Conectado com sucesso!")
 }).catch(function(erro){
     console.log("Falha ao se conectar: "+erro)
 })
+*/
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    }, 
+    conteudo: {
+        type: Sequelize.TEXT
+    }
+})
+
+Postagem.sync({force: true})
