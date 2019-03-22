1b5e9967d8c41aa4cad4ab0f90ea95f2c07f8790
Everaldo <everaldofront@gmail.com>
2019-03-22 12:20:09 -0300

    Models no Sequelize

--- bdteste.js
+++ bdteste.js
@@ -21,4 +21,31 @@ const Postagem = sequelize.define('postagens', {
     }
 })
 
-Postagem.sync({force: true})
+Postagem.create({
+    titulo: "Um titulo qualquer",
+    conteudo: "Um conteudo qualquer"
+})
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+// Usuario.sync({force: true})
+
+Usuario.create({
+    nome: "Everaldo",
+    sobrenome: "Santos",
+    idade: 35,
+    email: "everaldo@email.com"
+})
