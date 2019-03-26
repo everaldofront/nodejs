f5d28ae9e5f45b2aa562dc95732c87d8a445a727
Everaldo <everaldofront@gmail.com>
2019-03-26 08:38:17 -0300

    Pegando dados do formulário

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser')
+const Post = require('./models/Post')
 
 
 // Config
@@ -12,12 +13,23 @@ const bodyParser = require('body-parser')
         app.use(bodyParser.urlencoded({extended: false}))
         app.use(bodyParser.json())
     // Rotas
+    app.get('/', function(req, res){
+        res.render('home')
+    })
+
     app.get('/cad', function(req, res){
         res.render('formulario')
     })
 
     app.post('/add', function(req, res){
-        res.send("Texto: "+req.body.titulo+" Conteudo: "+req.body.conteudo)
+        Post.create({
+            titulo: req.body.titulo,
+            conteudo: req.body.conteudo
+        }).then(function(){
+            res.redirect('/')
+        }).catch(function(erro){
+            res.send("Houve um erro: " + erro)
+        })
     })
 
 app.listen(8081, function(){
