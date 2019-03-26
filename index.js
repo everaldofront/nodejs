580b4b9f0571e8600eb29a59ec0fedd104172c86
Everaldo <everaldofront@gmail.com>
2019-03-26 09:52:04 -0300

    Listando dados do banco

--- index.js
+++ index.js
@@ -13,25 +13,27 @@ const Post = require('./models/Post')
         app.use(bodyParser.urlencoded({extended: false}))
         app.use(bodyParser.json())
     // Rotas
-    app.get('/', function(req, res){
-        res.render('home')
-    })
-
-    app.get('/cad', function(req, res){
-        res.render('formulario')
-    })
-
-    app.post('/add', function(req, res){
-        Post.create({
-            titulo: req.body.titulo,
-            conteudo: req.body.conteudo
-        }).then(function(){
-            res.redirect('/')
-        }).catch(function(erro){
-            res.send("Houve um erro: " + erro)
+        app.get('/', function(req, res){
+            Post.findAll({order: [['id', 'DESC']]}).then(function(posts){
+                res.render('home', {posts: posts})  
+            })
         })
-    })
 
-app.listen(8081, function(){
-    console.log("Servidor rodando na url http://localhost:8081");
-});
+        app.get('/cad', function(req, res){
+            res.render('formulario')
+        })
+
+        app.post('/add', function(req, res){
+            Post.create({
+                titulo: req.body.titulo,
+                conteudo: req.body.conteudo
+            }).then(function(){
+                res.redirect('/')
+            }).catch(function(erro){
+                res.send("Houve um erro: " + erro)
+            })
+        })
+
+        app.listen(8081, function(){
+            console.log("Servidor rodando na url http://localhost:8081");
+        });
