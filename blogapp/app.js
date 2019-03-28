d9c5788f339cff8d096ac3b0a0b05736d2494e48
Everaldo <everaldofront@gmail.com>
2019-03-28 17:12:59 -0300

    Arquivos estáticos

--- blogapp/app.js
+++ blogapp/app.js
@@ -4,6 +4,7 @@
     const bodyParser = require('body-parser')
     const app = express()
     const admin = require("./routes/admin")
+    const path = require("path")
     //const mongoose = require('mongoose);
 
 // Configurações
@@ -12,13 +13,15 @@
     app.use(bodyParser.json())
     // Handlebars
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set(' view engine', 'handlebars');
+    app.set('view engine', 'handlebars');
     // Mongoose
         // Em Breve
+    //Public
+    app.use(express.static(path.join(__dirname,"public")))
 // Rotas
     app.use('/admin', admin)
 // Outros
-const PORT = 8081
+const PORT = 8089
 app.listen(PORT,() => {
     console.log("Servidor rodando!")
 })
