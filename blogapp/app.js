1d58313f46b67d2e6bf1c64b7ed5b60e06c567ad
Everaldo <everaldofront@gmail.com>
2019-03-28 12:41:39 -0300

    Grupo de rotas no Express.js

--- blogapp/app.js
+++ blogapp/app.js
@@ -1,21 +1,22 @@
 //Carregando módulos
-    const express = require('express');
-    const handlebars = require('express-handlebars');
-    const bodyParser = require('body-parser');
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
     const app = express()
-//const mongoose = require('mongoose);
+    const admin = require("./routes/admin")
+    //const mongoose = require('mongoose);
 
 // Configurações
     // Body Parser
     app.use(bodyParser.urlencoded({extended: true}))
-    app.use(bodyParser.jason())
+    app.use(bodyParser.json())
     // Handlebars
     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
     app.set(' view engine', 'handlebars');
     // Mongoose
         // Em Breve
 // Rotas
-
+    app.use('/admin', admin)
 // Outros
 const PORT = 8081
 app.listen(PORT,() => {
