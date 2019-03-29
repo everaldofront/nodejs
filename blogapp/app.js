54a39f1e7a020fbd2630d15b6d8e9aa9ee2c0577
Everaldo <everaldofront@gmail.com>
2019-03-29 17:28:41 -0300

    Como validar formulários no Express.js

--- blogapp/app.js
+++ blogapp/app.js
@@ -6,27 +6,42 @@
     const admin = require("./routes/admin")
     const path = require("path")
     const mongoose = require("mongoose")
+    const session = require("express-session")
+    const flash = require("connect-flash")
 
 // Configurações
+    //Sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg")
+            req.locals.error_msg = req.flash("error_msg")
+            next()
+        })
     // Body Parser
-    app.use(bodyParser.urlencoded({extended: true}))
-    app.use(bodyParser.json())
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
     // Handlebars
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view engine', 'handlebars');
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars');
     // Mongoose
-    mongoose.Promise = global.Promise;
-    mongoose.connect("mongodb://localhost/blogapp").then(() => {
-        console.log("Conectado ao mongo")
-    }).catch((err) => {
-        console.log("Erro ao se conectar "+err)
-    })
+        mongoose.Promise = global.Promise;
+        mongoose.connect("mongodb://localhost/blogapp").then(() => {
+            console.log("Conectado ao mongo")
+        }).catch((err) => {
+            console.log("Erro ao se conectar "+err)
+        })
     //Public
-    app.use(express.static(path.join(__dirname,"public")))
+        app.use(express.static(path.join(__dirname,"public")))
 // Rotas
-    app.use('/admin', admin)
+        app.use('/admin', admin)
 // Outros
-const PORT = 8089
-app.listen(PORT,() => {
-    console.log("Servidor rodando!")
-})
+    const PORT = 8089
+    app.listen(PORT,() => {
+        console.log("Servidor rodando!")
+    })
