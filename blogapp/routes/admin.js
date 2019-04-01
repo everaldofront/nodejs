2a5942973d0e5cdc5b03bbda3acf14c8a0e0de11
Everaldo <everaldofront@gmail.com>
2019-04-01 16:58:51 -0300

    Formulário de postagens

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -98,4 +98,13 @@ router.post("/categorias/deletar", (req, res) => {
     })
 })
 
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().then((categorias) => {
+        res.render("admin/addpostagem", {categorias: categorias})        
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário")
+        res.redirect("/admin")
+    })
+})
+
 module.exports = router
