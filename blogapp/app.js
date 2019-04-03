30863b767ecd812b89d65d34b071894400b0e12a
Everaldo <everaldofront@gmail.com>
2019-04-03 18:20:28 -0300

    Registro de usuários

--- blogapp/app.js
+++ blogapp/app.js
@@ -12,6 +12,7 @@
     const Postagem = mongoose.model("postagens")
     require("./models/Categoria")
     const Categoria = mongoose.model("categorias")
+    const usuarios = require("./routes/usuario")
 
 // Configurações
     //Sessão
@@ -107,6 +108,7 @@
         })
 
 app.use('/admin', admin);
+app.use('/usuarios', usuarios)
 
 // Outros
     const PORT = 8089
